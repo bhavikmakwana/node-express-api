8ebbaf71c65c119e9aada398fdf6346d1b78a761
Bhavik Makwana <bhavikmakwana1993@gmail.com>
2019-06-15 20:49:33 +0530

    model added for mongodb

---
+++ models/EmployeeModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose'); 
+const Scheema = mongoose.Schema;
+//create EmployeeModel schema 
+const employeeSchema  = new Scheema({
+    name:{
+        type : String,
+        required: [true,'Name field is required '],
+    },
+    age:{
+        type : String,
+        
+    },
+    name:{
+        type : Boolean,
+        default :false
+    }
+});
+const empModel = mongoose.model('Employee',employeeSchema);
+module.exports = empModel;
